420ccdda92e03f2812190038bfaac43249c6ff04
Netrunner <netrunner4014@gmail.com>
2023-07-28 14:39:59 +0700

    related_search_query

--- src/components/myComponent.js
+++ src/components/myComponent.js
@@ -1,7 +1,8 @@
 import '../components/myComponent.scss';
 import { useEffect, useState} from "react";
 import axios from 'axios';
-const baseURL = "https://npm-trends-proxy.uidotdev.workers.dev/npm/downloads/point/last-week/react";
+import moment from 'moment/moment';
+const baseURL = "https://npm-trends-proxy.uidotdev.workers.dev/s/related?search_query%5B%5D=angular&search_query%5B%5D=react&search_query%5B%5D=vue&limit=10";
 function MyComponent() {
     const[data, setData] = useState([])
     useEffect(() => {
@@ -11,19 +12,27 @@ function MyComponent() {
     },[])
     return (
         <div>
-        <h1>Reactjs</h1>
+        <h1>related_search_query</h1>
         <table>
             <thead><tr>
-            <th>Downloads</th>
-            <th>Start</th>
-            <th>End</th>
+            <th>slug</th>
+            <th>view_count</th>
+            <th>search_count</th>
+            <th>created_at</th>
+            <th>updated_at</th>
         </tr></thead>
             <tbody>
-            {[data].map((item, index) =>{
+            {console.log(data)}
+            {data.map((id, index) =>{
+                id.created = moment(id.created_at).format("DD/MM/YYYY")
+                id.updated = moment(id.updated_at).format("DD/MM/YYYY")
+                {/* console.log(id.created_at) */}
                 return (<tr key={index}>
-            <td>{item.downloads}</td>
-            <td>{item.start}</td>
-            <td>{item.end}</td>
+            <td>{id.slug}</td>
+            <td>{id.view_count}</td>
+            <td>{id.search_count}</td>
+            <td>{id.created}</td>
+            <td>{id.updated}</td>
         </tr>)
             })}
             </tbody>
